ba12ab7d399320320e4d0a024de487289a967d96
t.hayamizu <t.hayamizu@outlook.com>
2013-10-12 20:14:50 +0900

    modify directgraphics10definition.h directgraphics11definition.h

---
+++ Nyx/Source/Graphics/DX10/DirectGraphics10Definition.h
@@ -0,0 +1,22 @@
+#ifndef NYX_CORE_INCLUDED_DIRECT_GRAPHICS_10_DEFINITION_H_
+#define NYX_CORE_INCLUDED_DIRECT_GRAPHICS_10_DEFINITION_H_
+#include "boost/intrusive_ptr.hpp"
+
+
+//DXGI
+#include <DXGI.h>
+
+//D2D
+#include <D2D1.h>
+#include <D2D1Helper.h>
+#include <DWrite.h>
+
+//D3D
+#include <D3D10.h>
+#include <D3DX10.h>
+#include <DxErr.h>
+#pragma comment( lib, "d3d10.lib")
+#pragma comment( lib, "d3dx10.lib") 
+
+
+#endif
---
+++ Nyx/Source/Graphics/DX11/DirectGraphics11Definition.h
@@ -0,0 +1,23 @@
+#ifndef NYX_CORE_INCLUDED_DIRECT_GRAPHICS_11_DEFINITION_H_
+#define NYX_CORE_INCLUDED_DIRECT_GRAPHICS_11_DEFINITION_H_
+
+#include "boost/intrusive_ptr.hpp"
+
+
+//DXGI
+#include <DXGI.h>
+
+//D2D
+#include <D2D1.h>
+#include <D2D1Helper.h>
+#include <DWrite.h>
+
+//D3D
+#include <D3D11.h>
+#include <D3DX11.h>
+#include <DxErr.h>
+#pragma comment( lib, "d3d11.lib")
+#pragma comment( lib, "d3dx11.lib") 
+
+
+#endif
