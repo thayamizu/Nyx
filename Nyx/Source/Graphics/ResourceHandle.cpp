72c8d788ba13046089fdbbf55952a4051a6b8d19
t.hayamizu <t.hayamizu@outlook.com>
2013-10-29 23:17:43 +0900

    implements resrourcehandle class.
    Former-commit-id: 16827f8e8196a9adabadd73c9918e825743eaf51

--- Nyx/Source/Graphics/ResourceHandle.cpp
+++ Nyx/Source/Graphics/ResourceHandle.cpp
@@ -1,9 +1,27 @@
 #include "PCH/PCH.h"
+#include "Debug/DebugOutput.h"
 #include "Graphics/ResourceHandle.h"
+#include "boost/uuid/uuid.hpp"
+#include "boost/uuid/uuid_io.hpp"
+#include "boost/uuid/uuid_generators.hpp"
+#include "boost/lexical_cast.hpp"
 
 namespace Nyx  {
 
-	ResourceHandle::ResourceHandle() {
-	
+	ResourceHandle::ResourceHandle(size_t bufferSize, std::wstring guid, std::shared_ptr<void> resource)
+		: bufferSize_(bufferSize), uuid_(L""), resource_(resource)
+	{
+		using namespace boost;
+		using namespace boost::uuids;
+
+		try {
+			auto guid = random_generator()();
+			uuid_ = lexical_cast<std::wstring>(guid);
+		}
+		catch (std::exception e) {
+			DebugOutput::Trace("bad cast[%s:%s]", __FILE__, __LINE__);
+			throw e;
+		}
 	}
+
 }
