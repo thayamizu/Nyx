06077477ad8a9f9b69bffeee2dd66bc8c1f36ad3
t.hayamizu <t.hayamizu@outlook.com>
2013-10-29 23:18:00 +0900

    implements resourcecahce class
    Former-commit-id: 89beb08151d19731b7bf24a2ce6a9d3368262802

--- Nyx/Source/Graphics/ResourceCache.h
+++ Nyx/Source/Graphics/ResourceCache.h
@@ -3,9 +3,20 @@
 
 namespace Nyx
 {
+	class ResourceHandle;
 	class ResourceCache {
+		typedef std::unordered_map<std::wstring, std::shared_ptr<ResourceHandle>> Cache;
 	public:
 		ResourceCache();
+
+		void Add(const std::shared_ptr<ResourceHandle> handle);
+
+		void Delete(std::wstring key);
+
+		void Clear();
+
+	private:
+		Cache cache_;
 	};
 }
 
