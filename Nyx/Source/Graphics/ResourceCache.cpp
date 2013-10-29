7ce24b19198bac0dddc8a20f3beaf54c7b227066
t.hayamizu <t.hayamizu@outlook.com>
2013-10-29 23:18:00 +0900

    implements resourcecahce class
    Former-commit-id: af0ee959b15ad4b54dc712f15a5d57c688f6bac7

--- Nyx/Source/Graphics/ResourceCache.cpp
+++ Nyx/Source/Graphics/ResourceCache.cpp
@@ -1,9 +1,30 @@
 #include "PCH/PCH.h"
+#include "Debug/Assert.h"
+#include "Debug/DebugOutput.h"
 #include "Graphics/ResourceCache.h"
+#include "Graphics/ResourceHandle.h"
 
 namespace Nyx {
 
-	ResourceCache::ResourceCache() {
-	
+	ResourceCache::ResourceCache()
+		:cache_() {
 	}
+
+	void ResourceCache::Add(std::shared_ptr<ResourceHandle> handle)  {
+		Assert(handle != nullptr);
+		auto key = handle->GetId();
+		cache_[key] = handle;
+	}
+
+	void ResourceCache::Delete(std::wstring key) {
+		auto it = cache_.find(key);
+		if (it != cache_.end()) {
+			cache_.erase(it);
+		} 
+	}
+
+	void ResourceCache::Clear() {
+		cache_.clear();
+	}
+
 }
