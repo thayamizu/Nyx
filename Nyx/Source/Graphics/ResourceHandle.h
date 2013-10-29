f97f9d5f32c587353a4bccd68a0e0a0eff18ff2a
t.hayamizu <t.hayamizu@outlook.com>
2013-10-29 23:17:43 +0900

    implements resrourcehandle class.
    Former-commit-id: 661ca57f1452d6a80f614a271cc77df5eccb4a71

--- Nyx/Source/Graphics/ResourceHandle.h
+++ Nyx/Source/Graphics/ResourceHandle.h
@@ -5,7 +5,23 @@ namespace Nyx
 {
 	class ResourceHandle {
 	public:
-		ResourceHandle();
+		ResourceHandle(size_t bufferSize, std::wstring guid, std::shared_ptr<void> resource);
+
+		size_t GetBufferSize() {
+			return bufferSize_;
+		}
+
+		std::wstring GetId() const {
+			return uuid_;
+		}
+
+		std::shared_ptr<void> GetResource() const {
+			return resource_;
+		}
+	private:
+		size_t bufferSize_;
+		std::wstring uuid_;
+		std::shared_ptr<void> resource_;
 	};
 }
 
