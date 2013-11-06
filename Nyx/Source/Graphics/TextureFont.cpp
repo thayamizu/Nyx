e1c3743d6d257998a799c595a677cb9aa6643a1d
t.hayamizu <t.hayamizu@outlook.com>
2013-11-07 01:59:39 +0900

    ITextureFontをTextureFontにリネーム
    Former-commit-id: 4f28e94e69e515103cee948c458c6236ca094164

---
+++ Nyx/Source/Graphics/TextureFont.cpp
@@ -0,0 +1,38 @@
+/********************************************************************************
+*
+*  The MIT License
+*
+* Copyright (c)2010 -  t.hayamizu
+*
+*以下に定める条件に従い、本ソフトウェアおよび関連文書のファイル（以下「ソフトウェア」）の複製を取得するすべての人に対し、ソフトウェ
+*アを無制限に扱うことを無償で許可します。これには、ソフトウェアの複製を使用、複写、変更、結合、掲載、頒布、サブライセンス、および/ま
+*たは販売する権利、およびソフトウェアを提供する相手に同じことを許可する権利も無制限に含まれます。
+*
+*上記の著作権表示および本許諾表示を、ソフトウェアのすべての複製または重要な部分に記載するものとします。
+*
+*ソフトウェアは「現状のまま」で、明示であるか暗黙であるかを問わず、何らの保証もなく提供されます。ここでいう保証とは、商品性、特定の目*的への適合性、および権利非侵害についての保証も含みますが、それに限定されるものではありません。 作者または著作権者は、契約行為、不法
+*行為、またはそれ以外であろうと、ソフトウェアに起因または関連し、あるいはソフトウェアの使用またはその他の扱いによって生じる一切の請
+*求、損害、その他の義務について何らの責任も負わないものとします。 
+********************************************************************************/
+#include "PCH/PCH.h"
+#include "Graphics/TextureFont.h"
+
+namespace Nyx {
+	struct TextureFont::PImpl
+	{
+		FontInfo fontInfo_;
+	};
+	TextureFont::TextureFont(char character, const FontInfo& fontInfo) {
+
+
+	}
+	inline void TextureFont::SetFontInfo(const FontInfo& fontInfo){
+		pimpl_->fontInfo_ = fontInfo;
+	}
+	inline FontInfo TextureFont::GetFontInfo() const{
+		return pimpl_->fontInfo_;
+	}
+	inline void TextureFont::Render(const Matrix44& matrix){
+
+	}
+}
