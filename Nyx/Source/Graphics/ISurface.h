921ccbdb206c7f547700531d668206b7927823d2
t.hayamizu <t.hayamizu@outlook.com>
2013-11-07 23:03:27 +0900

    ISurfaceインタフェースを追加
    Former-commit-id: a9ffdc5d0262bbe0ab63a71511a6ee56af34a936

--- Nyx/Source/Graphics/ISurface.h
+++ Nyx/Source/Graphics/ISurface.h
@@ -1,15 +1,29 @@
 #ifndef NYX_CORE_INCLUDED_ISURFACE_H_
 #define NYX_CORE_INCLUDED_ISURFACE_H_
+#include "Primitive/Rect.h"
+#include "IRenderable.h"
 
 namespace Nyx {
-	class ISurface {
-	public:
-		ISurface() {}
-		~ISurface() {}
 
-	private:
-		int imagesize;
-		void* surfacebuffer
+	///•`‰æ‰Â”\‚È‹éŒ`—Ìˆæ
+	class ISurface : public IRenderable {
+	public:
+		/**
+		*
+		*/
+		virtual ~ISurface() {}
+
+
+		/**
+		*
+		*/
+		virtual void SetRect(const Rect2i& rect) = 0;
+
+
+		/**
+		*
+		*/
+		virtual Rect2i GetRect() const = 0;
 	};
 }
 #endif
