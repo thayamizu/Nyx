ad390a52a9eb28e98299368e69aaa1ef8984dd61
t.hayamizu <t.hayamizu@outlook.com>
2013-11-10 01:22:57 +0900

    SpriteManagerクラスの実装を変更
    Former-commit-id: 45ccb71fd4be9f64c0a1d25c4cfce01a79ff2239

--- Nyx/Source/Graphics/SpriteManager.h
+++ Nyx/Source/Graphics/SpriteManager.h
@@ -9,7 +9,7 @@ namespace Nyx {
 		SpriteManager();
 		std::shared_ptr<Sprite> CreateSprite(int width, int height, const Color4c& color);
 		std::shared_ptr<Sprite> CreateSprite(int width, int height, const std::wstring& fileName);
-		std::shared_ptr<SpriteCache> Load(const std::wstring& txtFileName, int width, int height);
+		std::shared_ptr<ResourceCache> Load(const std::wstring& txtFileName, int width, int height);
 	};
 }
 #endif
