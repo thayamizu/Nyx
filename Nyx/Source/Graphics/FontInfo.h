d63dcbc197d46956db8bc0b278e004d5fac773c8
t.hayamizu <t.hayamizu@outlook.com>
2013-11-10 01:13:32 +0900

    FontInfoクラスを追加
    Former-commit-id: e9511a46630e24ca0f98e6922fb2ff9dc357640b

---
+++ Nyx/Source/Graphics/FontInfo.h
@@ -0,0 +1,20 @@
+#ifndef NYX_CORE_INCLUDED_FONT_INFO_H_
+#define NYX_CORE_INCLUDED_FONT_INFO_H_
+#include "Primitive/Color4.h"
+
+namespace Nyx {
+
+	///フォント情報
+	struct FontInfo
+	{
+		int			   fontSize;///フォントサイズ
+		std::wstring   fontFace;///フォントフェイス
+		Color4c fontColor;///フォントカラー
+	};
+
+	class FontCache {
+
+
+	};
+}
+#endif
