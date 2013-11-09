0f00882880ade7ab4ab0e134ec13d31f3b7f90eb
t.hayamizu <t.hayamizu@outlook.com>
2013-11-10 03:12:14 +0900

    SpriteManagerの実装を修正
    Former-commit-id: 89fd023cc400efbb4d2f05a00f89126f038ff4a5

--- Nyx/Source/Graphics/SpriteManager.cpp
+++ Nyx/Source/Graphics/SpriteManager.cpp
@@ -1,32 +1,52 @@
 #include "PCH/PCH.h"
 #include "Sprite.h"
 #include "SpriteManager.h"
-#include "ResourceCache.h"
-#include "ResourceHandle.h"
+#include "detail/DX9/DirectXDefinition.h"
 
 namespace Nyx {
-	SpriteManager::SpriteManager() {}
+	//----------------------------------------------------------------------------------------
+	//
+	SpriteManager::SpriteManager() {
+	}
 
 
+	//----------------------------------------------------------------------------------------
+	//
 	std::shared_ptr<Sprite> SpriteManager::CreateSprite(int width, int height, const Color4c& color) {
-		return std::make_shared<Sprite>(width, height, color);
+		auto cache = D3d9ResourceCache::GetCache();
+		auto resource = std::make_shared<Sprite>(width, height, color);
+		
+		cache->Add(std::make_shared<ResourceHandle>(L"0", resource));
+		return resource;
 	}
 
-	std::shared_ptr<Sprite> SpriteManager::CreateSprite( int width, int height, const std::wstring& fileName) {
-		return std::make_shared<Sprite>(width, height, fileName);
+	//----------------------------------------------------------------------------------------
+	//
+	std::shared_ptr<Sprite> SpriteManager::CreateSprite(int width, int height, const std::wstring& fileName)
+	{
+		auto cache = D3d9ResourceCache::GetCache();
+		auto resource = std::make_shared<Sprite>(width, height, fileName);
+
+		cache->Add(std::make_shared<ResourceHandle>(fileName, resource));
+		return resource;
 	}
 
-	std::shared_ptr<ResourceCache> SpriteManager::Load(const std::wstring& fileName, int width, int height) {
-		auto cache = std::make_shared<ResourceCache>();
+
+	//----------------------------------------------------------------------------------------
+	//
+	SpriteCache  SpriteManager::Load(const std::wstring& fileName, int width, int height) {
+		auto d3dcache = D3d9ResourceCache::GetCache();///d3dƒŠƒ\[ƒX‚ÌŠÇ—
+		SpriteCache cache;
 
 		std::wstring line;
 		std::wifstream ifs;
 		ifs.open(fileName);
 		while (ifs >> line) {
 			auto resource = std::make_shared<Sprite>(width, height, line);
-
-			cache->Add(std::make_shared<ResourceHandle>(line, resource));
+			d3dcache->Add(std::make_shared<ResourceHandle>(line, resource));
+			cache.push_back(resource);
 		}
+
 		return cache;
 	}
 }
--- Nyx/Source/Graphics/SpriteManager.h
+++ Nyx/Source/Graphics/SpriteManager.h
@@ -4,12 +4,42 @@
 
 namespace Nyx {
 
+	typedef std::vector <std::shared_ptr<Sprite>> SpriteCache;
+
+	///スプライトマネージャ
 	class SpriteManager {
 	public:
+		/**
+		* コンストラクタ
+		*/
 		SpriteManager();
+
+		/**
+		* スプライトを生成します
+		* @param int スプライトの幅
+		* @param int スプライトの高さ
+		* @param std::wstring ファイル名
+		* @return std::shared_ptr<Sprite>
+		*/
 		std::shared_ptr<Sprite> CreateSprite(int width, int height, const Color4c& color);
+		
+		/**
+		* スプライトを生成します
+		* @param int スプライトの幅
+		* @param int スプライトの高さ
+		* @param const Color4c& 色
+		* @return std::shared_ptr<Sprite>
+		*/
 		std::shared_ptr<Sprite> CreateSprite(int width, int height, const std::wstring& fileName);
-		std::shared_ptr<ResourceCache> Load(const std::wstring& txtFileName, int width, int height);
+		
+		/**
+		* テキストファイルからまとめてキャッシュにロードします
+		* @param const std::wstring& テキストファイル名
+		* @param int スプライトの幅
+		* @param int スプライトの高さ
+		* @return std::vector<std::shared_ptr<Sprite>>
+		*/
+		SpriteCache Load(const std::wstring& txtFileName, int width, int height);
 	};
 }
 #endif
