cfe6af1ed73d9897d885056f4210c76be6edc568
t.hayamizu <t.hayamizu@outlook.com>
2014-03-25 23:28:37 +0900

    ファイルを追加
    Former-commit-id: a870eac6629d115e268b568ae32f4ffbb1eece37

---
+++ Nyx/Source/Graphics/Box.h
@@ -0,0 +1,17 @@
+#ifndef NYX_CORE_INCLUDED_BOX_H_
+#define NYX_CORE_INCLUDED_BOX_H_
+
+namespace Nyx {
+
+	template<typename T>
+	struct Box {
+		T minX, maxX;
+		T minY, minY;
+		T minZ, maxZ;
+	};
+
+	
+
+	
+}
+#endif
---
+++ Nyx/Source/Graphics/ParticleSystem.cpp
@@ -0,0 +1,15 @@
+#include "PCH/PCH.h"
+#include "ParticleSystem.h"
+
+namespace Nyx {
+	ParticleSystem::ParticleSystem() {
+	}
+	void ParticleSystem::CreateParticle()
+	{
+	}
+	void ParticleSystem::Update()
+	{
+	}
+	void ParticleSystem::Render(const Matrix44& world){
+	}
+}
