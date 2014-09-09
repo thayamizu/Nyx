bb6e8e2e323f1f1527a0e6d019b8081501bcdf85
t.hayamizu <t.hayamizu@outlook.com>
2014-09-09 23:37:30 +0900

    remove collision/bulletDefinition.h.

--- Nyx/Source/Collision/BulletDefinition.h
+++
@@ -1,9 +0,0 @@
-#ifndef NYX_CORE_INCLUDED_COLLISION_BULLET_DEFINITION_H_
-#define NYX_CORE_INCLUDED_COLLISION_BULLET_DEFINITION_H_
-
-#include <btCollisionCommon.h>
-#include <btPhisicsCommon.h>
-namespace nyx {
-	
-}
-#endif
